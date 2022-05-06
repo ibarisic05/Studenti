bba0dc5b48a3d9a7adaa7bef6f3b48b403b54e7b
Ivan Barisic <ivan.barisic@gauss.hr>
2022-05-06 15:32:13 +0200

    feat: added missing files

--- LV2-strucni/EntryViewController.swift
+++ LV2-strucni/EntryViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class EntryViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var imeLabel: UILabel!
+    @IBOutlet weak var prezimeLabel: UILabel!
+    @IBOutlet weak var imeTextField: UITextField!
+    @IBOutlet weak var prezimeTextField: UITextField!
+    @IBOutlet weak var createBtn: UIButton!
+    @IBOutlet weak var showBtn: UIButton!
+    
+    // MARK: - Variables
+    var studenti: Array<Student> = Array()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imeLabel.text = "Ime:"
+        prezimeLabel.text = "Prezime:"
+        
+        createBtn.setTitle("Novi student", for: .normal)
+        showBtn.setTitle("Prikazi studente", for: .normal)
     }
 
-
+    // MARK: - Actions
+    @IBAction func onTapCreateBtn(_ sender: Any) {
+        guard let ime = imeTextField.text, ime.count > 3 else {return}
+        guard let prezime = prezimeTextField.text, prezime.count > 3 else {return}
+        
+        studenti.append(Student(ime: ime, prezime: prezime))
+        imeTextField.text = nil
+        prezimeTextField.text = nil
+    }
+    
+    @IBAction func onTapShowBtn(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Lista studenata", message: nil, preferredStyle: .actionSheet)
+        
+        for student in studenti {
+            var actionTitle = "\(student.ime) \(student.prezime)"
+            if student.polozio {
+                actionTitle += " - Polozio"
+            }
+            
+            let studentAction = UIAlertAction(title: actionTitle, style: .default) { _ in
+                student.polozio = !student.polozio
+            }
+            alert.addAction(studentAction)
+        }
+        
+        alert.addAction(UIAlertAction(title: "Odustani", style: .cancel))
+        present(alert, animated: true)
+        
+    }
 }
 
--- LV2-strucni/Student.swift
+++ LV2-strucni/Student.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class Student {
+    let id: String = UUID().uuidString
+    let ime: String
+    let prezime: String
+    var polozio: Bool = false
+    
+    init(ime: String, prezime: String) {
+        self.ime = ime
+        self.prezime = prezime
+    }
+}
