9d36adef049df160e01f9d4093a4094e163e7dfa
Ivan Barisic <ivan.barisic@gauss.hr>
2022-05-06 15:18:50 +0200

    Initial Commit

---
+++ LV2-strucni/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LV2-strucni
+//
+//  Created by Ivan Barisic on 06.05.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
